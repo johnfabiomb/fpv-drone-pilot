49ed8611a3280bd70a060c6b86c2fa058478506b
John Montano <john.montano@lumation.co>
2024-05-27 15:42:25 +0200

    VERSION 3

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -7,15 +7,14 @@ import { MatDialog } from '@angular/material/dialog';
 import {
   clickOnMapPin,
   createMap,
-  createMapPin,
-  createVectorLayer,
   createView,
   getCoordinatesfromLonLat,
   getCoordinatesfromPixel,
 } from './map-functions';
 import { MapModalComponent, ModalActions } from '../map-modal/map-modal.component';
 import { locations } from '../../../assets/locations.json';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Overlay } from 'ol';
 
 @Component({
   selector: 'app-map',
@@ -25,8 +24,13 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class MapComponent implements AfterViewInit {
   public map!: Map;
   private maltaCoordinates = [14.363354400245052, 35.95195406978092]; // Central coordinates for Malta
+  locations = locations.reverse();
 
-  constructor(public dialog: MatDialog, public activatedRoute:ActivatedRoute) {
+  constructor(
+    public dialog: MatDialog, 
+    public activatedRoute:ActivatedRoute,
+    public router: Router
+  ) {
     
   }
 
@@ -39,26 +43,30 @@ export class MapComponent implements AfterViewInit {
     );
 
     // Create map pins for each location stored in the locations array
-    const pins = locations.map((location, index) => 
-      createMapPin(
-        getCoordinatesfromLonLat(location.lon, location.lat),
-        index,
-        location.img
-      )
+    locations.forEach((location, index) => {
+        var overlayelement = new Overlay({
+          stopEvent: false,
+          positioning: 'bottom-center',
+          element: document.getElementById(location.img) as HTMLElement
+        });
+        
+        overlayelement.setPosition( getCoordinatesfromLonLat(location.lon, location.lat));
+        this.map.addOverlay(overlayelement);
+
+      }
     );
 
     // Add a vector layer containing all pins to the map
-    const vectorLayer = createVectorLayer(pins);
-    this.map.addLayer(vectorLayer);
+    // const vectorLayer = createVectorLayer(pins);
+   
 
     // Set up event handling for clicking on map pins
     this.handleClickOnMapPin();
 
     this.activatedRoute.queryParams.subscribe((params:Params)=>{
-      if(params['id']){
-        const index = parseInt(params['id']);
-        const place = locations[parseInt(params['id'])];
-        this.openDialog(index, getCoordinatesfromLonLat(place.lon, place.lat))
+      if(params['title']){
+        const place = locations.find(loc=> encodeURIComponent(loc.title) === params['title']);
+        this.openDialog(place, getCoordinatesfromLonLat(place?.lon as number, place?.lat as number))
       }
     })
   }
@@ -72,28 +80,45 @@ export class MapComponent implements AfterViewInit {
 
       console.log(getCoordinatesfromPixel(flatCoordinates));
 
-      this.openDialog(num, flatCoordinates);
+      
     });
   }
 
-  private openDialog(num:number, flatCoordinates:number[]){
-    // Open a modal dialog with the location details when a pin is clicked
-    this.dialog.open(MapModalComponent, {
-      data: locations[num]
-    }).afterClosed().subscribe(res => {
-      // On modal close, check if the user wants to explore more (reset view)
-      if (res === ModalActions.EXPLORE) {
-        this.map.setView(
-          createView(this.getMaltaViewCoordinates(), 10.2)
-        );
+  clickon(data:any){
+    console.log(data)
+    const queryParams = { title: encodeURIComponent(data.title) };
+    this.router.navigate(
+      [], 
+      {
+        relativeTo: this.activatedRoute,
+        queryParams, 
+        queryParamsHandling: 'merge', // remove to replace all query params by provided
       }
-      if(res === ModalActions.GOOGLE_MAPS){
-        const url = "https://maps.google.com/?q="+  getCoordinatesfromPixel(flatCoordinates).reverse().join(',');
-        window.open(url, '_blank');
-      }
-    });
+    );
 
-    // Optionally adjust the map view to zoom closer to the selected pin
+  }
+
+  private openDialog(location:any, flatCoordinates:number[]){
+    setTimeout(() => {
+      
+      // Open a modal dialog with the location details when a pin is clicked
+      this.dialog.open(MapModalComponent, {
+        data: location
+      }).afterClosed().subscribe(res => {
+        // On modal close, check if the user wants to explore more (reset view)
+        if (res === ModalActions.EXPLORE) {
+          this.map.setView(
+            createView(this.getMaltaViewCoordinates(), 10.2)
+          );
+        }
+        if(res === ModalActions.GOOGLE_MAPS){
+          const url = "https://maps.google.com/?q="+  getCoordinatesfromPixel(flatCoordinates).reverse().join(',');
+          window.open(url, '_blank');
+        }
+      });
+    }, 1);
+
+    // // Optionally adjust the map view to zoom closer to the selected pin
     if ((this.map.getView().getZoom() ?? 0) <= 13) {
       this.map.setView(createView(flatCoordinates, 13));
     }
