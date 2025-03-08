e594acd05949cede8f3abf4b79aca87ac6c6a469
John Montano <john.montano@lumation.co>
2025-03-08 08:44:10 +0100

    new location added and new app style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { MaltaMapComponent } from './platform/malta-map/malta-map.component';
 export const routes: Routes = [
     {
         path: '',
+        pathMatch:'full',
+        redirectTo: 'malta'
+    },
+    {
+        path: '',
         loadComponent: () => import('./platform/home/home.component').then(mod => mod.HomeComponent)
     },
     {
